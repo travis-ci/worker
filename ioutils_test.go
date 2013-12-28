33dc28faa77e85eaf234356bc6c20c577851a18f
Henrik Hodne <henrik@hodne.io>
2013-12-28 08:20:35 +0100

    test(ioutils): improve test coverage for ioutils

--- ioutils_test.go
+++ ioutils_test.go
@@ -12,6 +12,10 @@ type testWriter struct {
 	closed  bool
 }
 
+func (t *testWriter) String() string {
+	return string(bytes.Join(t.written, []byte{}))
+}
+
 func (t *testWriter) Write(p []byte) (int, error) {
 	b := make([]byte, len(p))
 	copy(b, p)
@@ -24,7 +28,7 @@ func (t *testWriter) Close() error {
 	return nil
 }
 
-func TestCoalesceWriteCloser(t *testing.T) {
+func TestCoalesceWriteCloser_Write(t *testing.T) {
 	tw := &testWriter{written: make([][]byte, 0)}
 	cw := NewCoalesceWriteCloser(tw)
 
@@ -35,13 +39,19 @@ func TestCoalesceWriteCloser(t *testing.T) {
 	if !bytes.Equal(tw.written[0], []byte("hello world")) && len(tw.written) != 1 {
 		t.Errorf("expected writes to be coalesced, got %s", bytes.Join(tw.written, []byte(", ")))
 	}
+}
+
+func TestCoalesceWriteCloser_Close(t *testing.T) {
+	tw := &testWriter{written: make([][]byte, 0)}
+	cw := NewCoalesceWriteCloser(tw)
+	cw.Close()
 
 	if !tw.closed {
 		t.Error("expected Close() to propagate")
 	}
 }
 
-func TestTimeoutWriter(t *testing.T) {
+func TestTimeoutWriter_Timeout(t *testing.T) {
 	testWriter := &testWriter{written: make([][]byte, 0)}
 	tw := NewTimeoutWriter(testWriter, 2*time.Millisecond)
 	fmt.Fprint(tw, "hello world")
@@ -55,7 +65,29 @@ func TestTimeoutWriter(t *testing.T) {
 	}
 }
 
-func TestLimitWriter(t *testing.T) {
+func TestTimeoutWriter_Write(t *testing.T) {
+	testWriter := &testWriter{written: make([][]byte, 0)}
+	tw := NewTimeoutWriter(testWriter, 2*time.Millisecond)
+	defer tw.Close()
+
+	fmt.Fprint(tw, "hello world")
+
+	if testWriter.String() != "hello world" {
+		t.Errorf("expected written string to propagage, %q was written", testWriter.String())
+	}
+}
+
+func TestTimeoutWriter_Close(t *testing.T) {
+	testWriter := &testWriter{written: make([][]byte, 0)}
+	tw := NewTimeoutWriter(testWriter, 2*time.Millisecond)
+	tw.Close()
+
+	if !testWriter.closed {
+		t.Error("expected Close() to propagate")
+	}
+}
+
+func TestLimitWriter_LimitReached(t *testing.T) {
 	tw := &testWriter{written: make([][]byte, 0)}
 	lw := NewLimitWriter(tw, 10)
 	fmt.Fprintf(lw, "0123456789")
@@ -81,5 +113,27 @@ func TestLimitWriter(t *testing.T) {
 
 	if !limitReached {
 		t.Error("expected limit to have been reached after writing n+1 bytes")
+	}
+}
+
+func TestLimitWriter_Write(t *testing.T) {
+	tw := &testWriter{written: make([][]byte, 0)}
+	lw := NewLimitWriter(tw, 10)
+	defer lw.Close()
+
+	fmt.Fprint(lw, "hello world")
+
+	if tw.String() != "hello world" {
+		t.Errorf("expected written string to propagage, %q was written", tw.String())
+	}
+}
+
+func TestLimitWriter_Close(t *testing.T) {
+	tw := &testWriter{written: make([][]byte, 0)}
+	lw := NewLimitWriter(tw, 10)
+	lw.Close()
+
+	if !tw.closed {
+		t.Error("expected Close() to propagate")
 	}
 }
