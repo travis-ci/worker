e312dbb30aa7910f5fe984cccd7838f5c2757179
Henrik Hodne <henrik@travis-ci.com>
2015-01-22 16:48:13 +0100

    job: add more fields to the payloads

--- lib/job.go
+++ lib/job.go
@@ -8,24 +8,40 @@ import (
 
 // JobPayload is the payload we receive over RabbitMQ.
 type JobPayload struct {
-	Type       string            `json:"type"`
-	Job        JobJobPayload     `json:"job"`
-	Build      BuildPayload      `json:"source"`
-	Repository RepositoryPayload `json:"repository"`
-	UUID       string            `json:"uuid"`
+	Type       string                 `json:"type"`
+	Job        JobJobPayload          `json:"job"`
+	Build      BuildPayload           `json:"source"`
+	Repository RepositoryPayload      `json:"repository"`
+	UUID       string                 `json:"uuid"`
+	Config     map[string]interface{} `json:"config"`
 }
 
 type JobJobPayload struct {
-	ID uint64 `json:"id"`
+	ID               uint64 `json:"id"`
+	Number           string `json:"number"`
+	Commit           string `json:"commit"`
+	CommitRange      string `json:"commit_range"`
+	CommitMessage    string `json:"commit_message"`
+	Branch           string `json:"branch"`
+	State            string `json:"state"`
+	SecureEnvEnabled bool   `json:"secure_env_enabled"`
+	PullRequest      bool   `json:"pull_request"`
 }
 
 type BuildPayload struct {
-	ID uint64 `json:"id"`
+	ID     uint64 `json:"id"`
+	Number string `json:"number"`
 }
 
 type RepositoryPayload struct {
-	ID   uint64 `json:"id"`
-	Slug string `json:"slug"`
+	ID              uint64 `json:"id"`
+	Slug            string `json:"slug"`
+	GitHubID        uint64 `json:"github_id"`
+	SourceURL       string `json:"source_url"`
+	ApiURL          string `json:"api_url"`
+	LastBuildID     uint64 `json:"last_build_id"`
+	LastBuildNumber string `json:"last_build_number"`
+	Description     string `json:"description"`
 }
 
 // FinishState is the state that a job finished with (such as pass/fail/etc.).
