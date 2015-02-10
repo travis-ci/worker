89a3004c4224f7dd062114650e71bb8f100f30b1
Henrik Hodne <henrik@travis-ci.com>
2015-02-10 11:34:07 +0000

    update-state: only update the state if it was set

--- lib/step_update_state.go
+++ lib/step_update_state.go
@@ -18,14 +18,18 @@ func (s *stepUpdateState) Run(state multistep.StateBag) multistep.StepAction {
 func (s *stepUpdateState) Cleanup(state multistep.StateBag) {
 	buildJob := state.Get("buildJob").(Job)
 
-	result := state.Get("scriptResult").(backend.RunResult)
+	mresult, ok := state.GetOk("scriptResult")
 
-	switch result.ExitCode {
-	case 0:
-		buildJob.Finish(FinishStatePassed)
-	case 1:
-		buildJob.Finish(FinishStateFailed)
-	default:
-		buildJob.Finish(FinishStateErrored)
+	if ok {
+		result := mresult.(backend.RunResult)
+
+		switch result.ExitCode {
+		case 0:
+			buildJob.Finish(FinishStatePassed)
+		case 1:
+			buildJob.Finish(FinishStateFailed)
+		default:
+			buildJob.Finish(FinishStateErrored)
+		}
 	}
 }
