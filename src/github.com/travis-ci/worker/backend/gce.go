9f6720e6b856b69c5439d4f6de7e43095b7105f1
Dan Buch <dan@travis-ci.org>
2015-07-04 00:57:57 -0400

    More work on GCE backend
    stumped on RSA private key issue :scream_cat:

--- src/github.com/travis-ci/worker/backend/gce.go
+++ src/github.com/travis-ci/worker/backend/gce.go
@@ -1,40 +1,107 @@
 package backend
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/pborman/uuid"
 	"github.com/pkg/sftp"
 	"github.com/travis-ci/worker/config"
+	"github.com/travis-ci/worker/context"
+	"github.com/travis-ci/worker/metrics"
 	"golang.org/x/crypto/ssh"
 	gocontext "golang.org/x/net/context"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
 	"google.golang.org/api/compute/v1"
 )
 
 const (
-	gceHelp = `
-              CLIENT_ID - [REQUIRED] client id for OAuth2
-          CLIENT_SECRET - [REQUIRED] client secret for OAuth2
-                  TOKEN - [REQUIRED] token for OAuth2
-
+	defaultGCEZone        = "us-central1-a"
+	defaultGCEMachineType = "n1-standard-2"
+	defaultGCENetwork     = "default"
+	defaultGCEDiskSize    = int64(20)
+	gceImagesFilter       = "name eq ^travis-ci-%s.+"
+	gceStartupScript      = `#!/usr/bin/env bash
+cat > ~travis/.ssh/authorized_keys <<EOF
+%s
+EOF
 `
+)
+
+var (
+	gceHelp = fmt.Sprintf(`
+             PROJECT_ID - [REQUIRED] GCE project id
+           ACCOUNT_JSON - [REQUIRED] account JSON config
+           SSH_KEY_PATH - [REQUIRED] path to ssh key used to access job vms
+     SSH_KEY_PASSPHRASE - [REQUIRED] passphrase for ssh key given as ssh_key_path
+                   ZONE - zone name (default %q)
+           MACHINE_TYPE - machine name (default %q)
+                NETWORK - machine name (default %q)
+              DISK_SIZE - disk size in GB (default %v)
+
+`, defaultGCEZone, defaultGCEMachineType, defaultGCENetwork, defaultGCEDiskSize)
+	gceMissingIPAddressError = fmt.Errorf("no IP address found")
 )
 
 func init() {
 	config.SetProviderHelp("GCE", gceHelp)
 }
 
+type gceOpError struct {
+	Err *compute.OperationError
+}
+
+func (oe *gceOpError) Error() string {
+	errStrs := []string{}
+	for _, err := range oe.Err.Errors {
+		errStrs = append(errStrs, fmt.Sprintf("code=%s location=%s message=%s",
+			err.Code, err.Location, err.Message))
+	}
+
+	return strings.Join(errStrs, ", ")
+}
+
+type gceAccountJSON struct {
+	ClientEmail string `json:"client_email"`
+	PrivateKey  string `json:"private_key"`
+}
+
 type GCEProvider struct {
-	client *compute.Service
+	client    *compute.Service
+	projectID string
+	ic        *gceInstanceConfig
+}
+
+type gceInstanceConfig struct {
+	MachineType      *compute.MachineType
+	Zone             *compute.Zone
+	Network          *compute.Network
+	DiskType         string
+	DiskSize         int64
+	SSHKeyPath       string
+	SSHKeyPassphrase string
 }
 
 type GCEInstance struct {
 	client   *compute.Service
 	provider *GCEProvider
 	instance *compute.Instance
+	ic       *gceInstanceConfig
 
+	authUser string
+	authKey  *rsa.PrivateKey
+
+	projectID string
 	imageName string
 }
 
@@ -44,173 +111,337 @@ func NewGCEProvider(cfg *config.ProviderConfig) (*GCEProvider, error) {
 		return nil, err
 	}
 
+	if !cfg.IsSet("PROJECT_ID") {
+		return nil, fmt.Errorf("mising PROJECT_ID")
+	}
+
+	projectID := cfg.Get("PROJECT_ID")
+
+	if !cfg.IsSet("SSH_KEY_PATH") {
+		return nil, fmt.Errorf("expected SSH_KEY_PATH config key")
+	}
+
+	sshKeyPath := cfg.Get("SSH_KEY_PATH")
+
+	if !cfg.IsSet("SSH_KEY_PASSPHRASE") {
+		return nil, fmt.Errorf("expected SSH_KEY_PASSPHRASE config key")
+	}
+
+	sshKeyPassphrase := cfg.Get("SSH_KEY_PASSPHRASE")
+
+	zoneName := defaultGCEZone
+	if cfg.IsSet("ZONE") {
+		zoneName = cfg.Get("ZONE")
+	}
+
+	zone, err := client.Zones.Get(projectID, zoneName).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	mtName := defaultGCEMachineType
+	if cfg.IsSet("MACHINE_TYPE") {
+		mtName = cfg.Get("MACHINE_TYPE")
+	}
+
+	mt, err := client.MachineTypes.Get(projectID, zone.Name, mtName).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	nwName := defaultGCENetwork
+	if cfg.IsSet("NETWORK") {
+		nwName = cfg.Get("NETWORK")
+	}
+
+	nw, err := client.Networks.Get(projectID, nwName).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	diskSize := defaultGCEDiskSize
+	if cfg.IsSet("DISK_SIZE") {
+		ds, err := strconv.ParseInt(cfg.Get("DISK_SIZE"), 10, 64)
+		if err == nil {
+			diskSize = ds
+		}
+	}
+
 	return &GCEProvider{
-		client: client,
+		client:    client,
+		projectID: projectID,
+		ic: &gceInstanceConfig{
+			MachineType: mt,
+			Zone:        zone,
+			Network:     nw,
+			// DiskType:         fmt.Sprintf("zones/%s/diskTypes/local-ssd", zone.Name),
+			DiskType:         fmt.Sprintf("zones/%s/diskTypes/pd-standard", zone.Name),
+			DiskSize:         diskSize,
+			SSHKeyPath:       sshKeyPath,
+			SSHKeyPassphrase: sshKeyPassphrase,
+		},
 	}, nil
 }
 
 func buildGoogleComputeService(cfg *config.ProviderConfig) (*compute.Service, error) {
-	if !cfg.IsSet("CLIENT_ID") {
-		return nil, fmt.Errorf("missing CLIENT_ID")
+	if !cfg.IsSet("ACCOUNT_JSON") {
+		return nil, fmt.Errorf("missing ACCOUNT_JSON")
 	}
 
-	if !cfg.IsSet("CLIENT_SECRET") {
-		return nil, fmt.Errorf("missing CLIENT_SECRET")
+	a, err := loadGoogleAccountJSON(cfg.Get("ACCOUNT_JSON"))
+	if err != nil {
+		return nil, err
 	}
 
-	config := &oauth2.Config{
-		ClientID:     cfg.Get("CLIENT_ID"),
-		ClientSecret: cfg.Get("CLIENT_SECRET"),
-		Endpoint:     google.Endpoint,
+	config := jwt.Config{
+		Email:      a.ClientEmail,
+		PrivateKey: []byte(a.PrivateKey),
 		Scopes: []string{
 			compute.DevstorageFullControlScope,
 			compute.ComputeScope,
 		},
+		TokenURL: "https://accounts.google.com/o/oauth2/token",
 	}
-	return compute.New(config.Client(gocontext.Background(), &oauth2.Token{
-		AccessToken: cfg.Get("TOKEN"),
-	}))
+	return compute.New(config.Client(oauth2.NoContext))
+}
+
+func loadGoogleAccountJSON(filename string) (*gceAccountJSON, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	a := &gceAccountJSON{}
+	err = json.Unmarshal(bytes, a)
+	return a, err
 }
 
 func (p *GCEProvider) Start(ctx gocontext.Context, startAttributes *StartAttributes) (Instance, error) {
-	/*
-		logger := context.LoggerFromContext(ctx)
-		imageName, err := p.imageForLanguage(startAttributes.Language)
-		if err != nil {
-			return nil, err
-		}
+	logger := context.LoggerFromContext(ctx)
+	image, err := p.imageForLanguage(startAttributes.Language)
+	if err != nil {
+		return nil, err
+	}
 
-		dockerConfig := &docker.Config{
-			Cmd:      p.runCmd,
-			Image:    imageID,
-			Memory:   int64(p.runMemory),
-			Hostname: fmt.Sprintf("testing-docker-%s", uuid.NewUUID()),
-		}
+	authKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
 
-		dockerHostConfig := &docker.HostConfig{
-			Privileged: p.runPrivileged,
-		}
+	pubKey, err := ssh.NewPublicKey(authKey)
+	if err != nil {
+		return nil, err
+	}
 
-		if cpuSets != "" {
-			dockerConfig.CPUSet = cpuSets
-		}
+	inst := &compute.Instance{
+		Description: fmt.Sprintf("Travis CI %s test VM", startAttributes.Language),
+		Disks: []*compute.AttachedDisk{
+			&compute.AttachedDisk{
+				Type:       "PERSISTENT",
+				Mode:       "READ_WRITE",
+				Boot:       true,
+				AutoDelete: true,
+				InitializeParams: &compute.AttachedDiskInitializeParams{
+					SourceImage: image.SelfLink,
+					DiskType:    p.ic.DiskType,
+					DiskSizeGb:  p.ic.DiskSize,
+				},
+			},
+		},
+		Scheduling: &compute.Scheduling{
+			Preemptible: true,
+		},
+		MachineType: p.ic.MachineType.SelfLink,
+		Name:        fmt.Sprintf("testing-gce-%s", uuid.NewUUID()),
+		Metadata: &compute.Metadata{
+			Items: []*compute.MetadataItems{
+				&compute.MetadataItems{
+					Key:   "startup-script",
+					Value: fmt.Sprintf(gceStartupScript, string(ssh.MarshalAuthorizedKey(pubKey))),
+				},
+			},
+		},
+		NetworkInterfaces: []*compute.NetworkInterface{
+			&compute.NetworkInterface{
+				AccessConfigs: []*compute.AccessConfig{
+					&compute.AccessConfig{
+						Name: "AccessConfig brought to you by travis-worker",
+						Type: "ONE_TO_ONE_NAT",
+					},
+				},
+				Network: p.ic.Network.SelfLink,
+			},
+		},
+		ServiceAccounts: []*compute.ServiceAccount{
+			&compute.ServiceAccount{
+				Email: "default",
+				Scopes: []string{
+					"https://www.googleapis.com/auth/userinfo.email",
+					compute.DevstorageFullControlScope,
+					compute.ComputeScope,
+				},
+			},
+		},
+		Tags: &compute.Tags{
+			Items: []string{
+				"testing",
+				startAttributes.Language,
+			},
+		},
+	}
 
-		logger.WithFields(logrus.Fields{
-			"config":      fmt.Sprintf("%#v", dockerConfig),
-			"host_config": fmt.Sprintf("%#v", dockerHostConfig),
-		}).Debug("starting container")
+	logger.WithFields(logrus.Fields{
+		"instance": inst,
+	}).Debug("inserting instance")
+	op, err := p.client.Instances.Insert(p.projectID, p.ic.Zone.Name, inst).Do()
+	if err != nil {
+		return nil, err
+	}
 
-		container, err := p.client.CreateContainer(docker.CreateContainerOptions{
-			Config:     dockerConfig,
-			HostConfig: dockerHostConfig,
-		})
+	startBooting := time.Now()
 
-		if err != nil {
-			if container != nil {
-				err := p.client.RemoveContainer(docker.RemoveContainerOptions{
-					ID:            container.ID,
-					RemoveVolumes: true,
-					Force:         true,
-				})
-				if err != nil {
-					logger.WithField("err", err).Error("couldn't remove container after create failure")
-				}
+	instanceReady := make(chan *compute.Instance)
+	errChan := make(chan error)
+	go func() {
+		for {
+			newOp, err := p.client.ZoneOperations.Get(p.projectID, p.ic.Zone.Name, op.Name).Do()
+			if err != nil {
+				errChan <- err
+				return
 			}
 
-			return nil, err
-		}
-
-		startBooting := time.Now()
-
-		err = p.client.StartContainer(container.ID, &docker.HostConfig{})
-		if err != nil {
-			return nil, err
-		}
-
-		containerReady := make(chan *docker.Container)
-		errChan := make(chan error)
-		go func(id string) {
-			for true {
-				container, err := p.client.InspectContainer(id)
-				if err != nil {
-					errChan <- err
+			if newOp.Status == "DONE" {
+				if newOp.Error != nil {
+					errChan <- &gceOpError{Err: newOp.Error}
 					return
 				}
 
-				if container.State.Running {
-					containerReady <- container
-					return
-				}
+				instanceReady <- inst
+				return
 			}
-		}(container.ID)
-
-		select {
-		case container := <-containerReady:
-			metrics.TimeSince("worker.vm.provider.docker.boot", startBooting)
-			return &GCEInstance{
-				client:    p.client,
-				provider:  p,
-				container: container,
-				imageName: imageName,
-			}, nil
-		case err := <-errChan:
-			return nil, err
-		case <-ctx.Done():
-			if ctx.Err() == gocontext.DeadlineExceeded {
-				metrics.Mark("worker.vm.provider.docker.boot.timeout")
-			}
-			return nil, ctx.Err()
 		}
+	}()
 
-	*/
-	return nil, nil
+	select {
+	case inst := <-instanceReady:
+		metrics.TimeSince("worker.vm.provider.gce.boot", startBooting)
+		return &GCEInstance{
+			client:   p.client,
+			provider: p,
+			instance: inst,
+			ic:       p.ic,
+
+			authUser: "travis",
+			authKey:  authKey,
+
+			projectID: p.projectID,
+			imageName: image.Name,
+		}, nil
+	case err := <-errChan:
+		return nil, err
+	case <-ctx.Done():
+		if ctx.Err() == gocontext.DeadlineExceeded {
+			metrics.Mark("worker.vm.provider.gce.boot.timeout")
+		}
+		return nil, ctx.Err()
+	}
 }
 
-func (p *GCEProvider) imageForLanguage(language string) (string, error) {
-	return "", nil
-	/*
-		images, err := p.client.ListImages(docker.ListImagesOptions{All: true})
-		if err != nil {
-			return "", "", err
-		}
+func (p *GCEProvider) imageForLanguage(language string) (*compute.Image, error) {
+	images, err := p.client.Images.List(p.projectID).Filter(fmt.Sprintf(gceImagesFilter, language)).Do()
+	if err != nil {
+		return nil, err
+	}
 
-		for _, image := range images {
-			for _, searchTag := range []string{
-				"travis:" + language,
-				language,
-				"travis:default",
-				"default",
-			} {
-				for _, tag := range image.RepoTags {
-					if tag == searchTag {
-						return image.ID, tag, nil
-					}
-				}
-			}
-		}
+	if len(images.Items) == 0 {
+		return nil, fmt.Errorf("no image found with language %s", language)
+	}
 
-		return "", "", fmt.Errorf("no image found with language %s", language)
-	*/
+	imagesByName := map[string]*compute.Image{}
+	imageNames := []string{}
+	for _, image := range images.Items {
+		imagesByName[image.Name] = image
+		imageNames = append(imageNames, image.Name)
+	}
+
+	sort.Strings(imageNames)
+
+	return imagesByName[imageNames[len(imageNames)-1]], nil
 }
 
 func (i *GCEInstance) sshClient() (*ssh.Client, error) {
 	/*
-		var err error
-		i.container, err = i.client.InspectContainer(i.container.ID)
+		file, err := ioutil.ReadFile(i.ic.SSHKeyPath)
 		if err != nil {
 			return nil, err
 		}
 
-		time.Sleep(2 * time.Second)
+		block, _ := pem.Decode(file)
+		if block == nil {
+			return nil, fmt.Errorf("ssh key does not contain a valid PEM block")
+		}
 
-		return ssh.Dial("tcp", fmt.Sprintf("%s:22", i.container.NetworkSettings.IPAddress), &ssh.ClientConfig{
-			User: "travis",
-			Auth: []ssh.AuthMethod{
-				ssh.Password("travis"),
-			},
-		})
+		der, err := x509.DecryptPEMBlock(block, []byte(i.ic.SSHKeyPassphrase))
+		if err != nil {
+			return nil, err
+		}
+
+		key, err := x509.ParsePKCS1PrivateKey(der)
+		if err != nil {
+			return nil, err
+		}
+
+		signer, err := ssh.NewSignerFromKey(key)
+		if err != nil {
+			return nil, err
+		}
 	*/
-	return nil, nil
+
+	i.refreshInstance()
+
+	ipAddr := i.getIP()
+	if ipAddr == "" {
+		return nil, gceMissingIPAddressError
+	}
+
+	// FIXME: i.authKey is *rsa.PrivateKey, but NewSignerFromKey rejects it??
+	ks, err := ssh.NewSignerFromKey(i.authKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.Dial("tcp", fmt.Sprintf("%s:22", ipAddr), &ssh.ClientConfig{
+		User: i.authUser,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(ks),
+		},
+	})
+}
+
+func (i *GCEInstance) getIP() string {
+	for _, ni := range i.instance.NetworkInterfaces {
+		if ni.AccessConfigs == nil {
+			continue
+		}
+
+		for _, ac := range ni.AccessConfigs {
+			if ac.NatIP != "" {
+				return ac.NatIP
+			}
+		}
+	}
+
+	return ""
+}
+
+func (i *GCEInstance) refreshInstance() error {
+	inst, err := i.client.Instances.Get(i.projectID, i.ic.Zone.Name, i.instance.Name).Do()
+	if err != nil {
+		return err
+	}
+
+	i.instance = inst
+	return nil
 }
 
 func (i *GCEInstance) UploadScript(ctx gocontext.Context, script []byte) error {
@@ -278,30 +509,40 @@ func (i *GCEInstance) RunScript(ctx gocontext.Context, output io.WriteCloser) (*
 }
 
 func (i *GCEInstance) Stop(ctx gocontext.Context) error {
-	/*
-		defer i.provider.checkinCPUSets(i.container.Config.CPUSet)
+	op, err := i.client.Instances.Delete(i.projectID, i.ic.Zone.Name, i.instance.Name).Do()
+	if err != nil {
+		return err
+	}
 
-		err := i.client.StopContainer(i.container.ID, 30)
-		if err != nil {
-			return err
+	errChan := make(chan error)
+	go func() {
+		for {
+			newOp, err := i.client.ZoneOperations.Get(i.projectID, i.ic.Zone.Name, op.Name).Do()
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if newOp.Status == "DONE" {
+				if newOp.Error != nil {
+					errChan <- &gceOpError{Err: newOp.Error}
+					return
+				}
+
+				errChan <- nil
+				return
+			}
 		}
+	}()
 
-		return i.client.RemoveContainer(docker.RemoveContainerOptions{
-			ID:            i.container.ID,
-			RemoveVolumes: true,
-			Force:         true,
-		})
-	*/
-	return nil
+	select {
+	case err := <-errChan:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
 
 func (i *GCEInstance) ID() string {
-	/*
-		if i.container == nil {
-			return "{unidentified}"
-		}
-
-		return fmt.Sprintf("%s:%s", i.container.ID[0:7], i.imageName)
-	*/
-	return ""
+	return fmt.Sprintf("%s:%s", i.instance.Name, i.imageName)
 }
