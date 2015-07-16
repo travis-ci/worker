589bb07b25a2dabc04e149fdca7c23d8339994e3
Brandon Burton <brandon@inatree.org>
2015-07-16 15:12:35 -0700

    Initial cut of pruning code for packages on packagecloud

---
+++ utils/pkg/packagecloud_prune.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+# from http://blog.packagecloud.io/api/2015/07/06/pruning-packages-using-the-API/
+
+require 'json'
+require 'pp'
+require 'rest-client'
+require 'time'
+
+if ENV['PACKAGECLOUD_TOKEN']
+  API_TOKEN = ENV['PACKAGECLOUD_TOKEN']
+else
+  puts "Set PACKAGECLOUD_TOKEN"
+  exit 1
+end
+
+if ENV['PC_USER']
+  USER = ENV['PC_USER']
+else
+  puts "Set PC_USER"
+  exit 1
+end
+
+if ARGV[0]
+  REPOSITORY = ARGV[0]
+else
+  puts "Supply the repository name as the first argument"
+  puts "E.g. 'worker' or 'worker-testing"
+  exit 1
+end
+
+PACKAGE = 'travis-worker'
+if ARGV[1]
+  DIST = ARGV[1]
+else
+  puts "Supply the distribution name+release as the second argument"
+  puts "E.g. 'ubuntu/trusty' or 'el/7'"
+  exit 1
+end
+
+if ARGV[2]
+  LIMIT = ARGV[2].to_i
+else
+  LIMIT = 10
+end
+
+if DIST.include?("ubuntu")
+  PACKAGE_TYPE = "deb"
+  PACKAGE_ARCH = "amd64"
+elsif DIST.include?("el")
+  PACKAGE_TYPE = "rpm"
+  PACKAGE_ARCH = "x86_64"
+end
+
+base_url = "https://#{API_TOKEN}:@packagecloud.io/api/v1/repos/#{USER}/#{REPOSITORY}"
+
+package_url = "/package/#{PACKAGE_TYPE}/#{DIST}/#{PACKAGE}/#{PACKAGE_ARCH}/versions.json"
+
+url = base_url + package_url
+package_versions = RestClient.get(url)
+
+parsed_package_versions = JSON.parse(package_versions)
+
+sorted_package_versions = parsed_package_versions.sort_by { |x| Time.parse(x["created_at"]) }.reverse!
+i = sorted_package_versions.size - 1
+puts "There are currently #{i} packages in #{REPOSITORY}"
+puts "Your LIMIT is #{LIMIT}"
+puts "Deleting #{i - LIMIT}"
+
+until i == LIMIT
+  to_yank = sorted_package_versions[i]
+
+  distro_version = to_yank["distro_version"]
+  filename = to_yank["filename"]
+  yank_url = "/#{distro_version}/#{filename}"
+  url = base_url + yank_url
+
+  puts "wuold be yanking #{filename}"
+  #result = RestClient.delete(url)
+  #if result == {}
+  #  puts "successfully yanked #{filename}!"
+  #else
+  #  puts "failed with #{result}"
+  #end
+  i -= 1
+end
