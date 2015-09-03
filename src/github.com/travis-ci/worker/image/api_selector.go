7d33d4fcd0e4b026518502bd63224265ba0915ba
Dan Buch <dan@travis-ci.org>
2015-09-03 12:51:48 -0400

    Func extraction and more tests for API image selector

--- src/github.com/travis-ci/worker/image/api_selector.go
+++ src/github.com/travis-ci/worker/image/api_selector.go
@@ -13,99 +13,148 @@ import (
 
 type APISelector struct {
 	baseURL *url.URL
+
+	maxInterval    time.Duration
+	maxElapsedTime time.Duration
 }
 
 func NewAPISelector(u *url.URL) *APISelector {
-	return &APISelector{baseURL: u}
+	return &APISelector{
+		baseURL: u,
+
+		maxInterval:    10 * time.Second,
+		maxElapsedTime: time.Minute,
+	}
 }
 
 func (as *APISelector) Select(params *Params) (string, error) {
-	imageName := "default"
+	for _, tags := range as.buildCandidateTags(params) {
+		imageName, err := as.queryWithTags(params.Infra, tags)
+		if err != nil {
+			return "default", err
+		}
 
+		if imageName != "" {
+			return imageName, nil
+		}
+	}
+
+	return "default", nil
+}
+
+func (as *APISelector) queryWithTags(infra string, tags []string) (string, error) {
+	u, _ := url.Parse(as.baseURL.String())
+	qs := url.Values{}
+	qs.Set("infra", infra)
+	qs.Set("limit", "1")
+	if len(tags) > 0 {
+		qs.Set("tags", strings.Join(tags, ","))
+	}
+
+	u.RawQuery = qs.Encode()
+
+	imageResp, err := as.makeImageRequest(u.String())
+	if err != nil {
+		return "", err
+	}
+
+	if len(imageResp.Data) > 0 {
+		return imageResp.Data[0].Name, nil
+	}
+
+	return "", nil
+}
+
+func (as *APISelector) makeImageRequest(urlString string) (*apiSelectorImageResponse, error) {
+	var body []byte
+
+	b := backoff.NewExponentialBackOff()
+	b.MaxInterval = 10 * time.Second
+	b.MaxElapsedTime = time.Minute
+
+	err := backoff.Retry(func() (err error) {
+		resp, err := http.Get(urlString)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		body, err = ioutil.ReadAll(resp.Body)
+		return
+	}, b)
+
+	if err != nil {
+		return nil, err
+	}
+
+	imageResp := &apiSelectorImageResponse{
+		Data: []*apiSelectorImageRef{},
+	}
+
+	err = json.Unmarshal(body, imageResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return imageResp, nil
+}
+
+func (as *APISelector) buildCandidateTags(params *Params) [][]string {
 	fullTagSet := []string{}
+	candidateTags := [][]string{}
 
-	if params.Language != "" {
-		fullTagSet = append(fullTagSet, "language:"+params.Language)
+	addTag := func(tag string) {
+		fullTagSet = append(fullTagSet, tag)
+		candidateTags = append(candidateTags, []string{tag})
+	}
+
+	addTags := func(tags ...string) {
+		candidateTags = append(candidateTags, tags)
+	}
+
+	hasLang := params.Language != ""
+
+	if params.OS == "osx" && params.OsxImage != "" && hasLang {
+		addTags("osx_image:"+params.OsxImage, "language:"+params.Language)
+		addTags("osx_image:"+params.OsxImage, "language_"+params.Language+":true")
+	}
+
+	if params.Dist != "" && hasLang {
+		addTags("dist:"+params.Dist, "language:"+params.Language)
+		addTags("dist:"+params.Dist, "language_"+params.Language+":true")
+	}
+
+	if params.Group != "" && hasLang {
+		addTags("group:"+params.Group, "language:"+params.Language)
+		addTags("group:"+params.Group, "language_"+params.Language+":true")
+	}
+
+	if params.OS != "" && hasLang {
+		addTags("os:"+params.OS, "language:"+params.Language)
+		addTags("os:"+params.OS, "language_"+params.Language+":true")
+	}
+
+	if hasLang {
+		addTag("language:" + params.Language)
+		addTag("language_" + params.Language + ":true")
+	}
+
+	if params.OS == "osx" && params.OsxImage != "" {
+		addTag("osx_image:" + params.OsxImage)
 	}
 
 	if params.Dist != "" {
-		fullTagSet = append(fullTagSet, "dist:"+params.Dist)
+		addTag("dist:" + params.Dist)
 	}
 
 	if params.Group != "" {
-		fullTagSet = append(fullTagSet, "group:"+params.Group)
+		addTag("group:" + params.Group)
 	}
 
 	if params.OS != "" {
-		fullTagSet = append(fullTagSet, "os:"+params.OS)
+		addTag("os:" + params.OS)
 	}
 
-	tagAttempts := [][]string{}
-
-	if params.OS == "osx" && params.OsxImage != "" {
-		fullTagSet = append(fullTagSet, "osx_image:"+params.OsxImage)
-		tagAttempts = append(tagAttempts,
-			[]string{"osx_image:" + params.OsxImage, "language:" + params.Language},
-			[]string{"osx_image:" + params.OsxImage})
-	}
-
-	tagAttempts = append(tagAttempts,
-		fullTagSet,
-		[]string{"language:" + params.Language},
-		[]string{"language_" + params.Language + ":true"},
-		[]string{"dist:" + params.Dist, "language:" + params.Language},
-		[]string{"dist:" + params.Dist, "language_" + params.Language + ":true"},
-		[]string{"dist:" + params.Dist},
-		[]string{"group:" + params.Group, "language:" + params.Language},
-		[]string{"group:" + params.Group, "language_" + params.Language + ":true"},
-		[]string{"group:" + params.Group},
-		[]string{"os:" + params.OsxImage, "language:" + params.Language},
-		[]string{"os:" + params.OsxImage, "language_" + params.Language + ":true"},
-		[]string{"os:" + params.OsxImage})
-
-	for _, tags := range tagAttempts {
-		u, _ := url.Parse(as.baseURL.String())
-		qs := url.Values{}
-		qs.Set("infra", params.Infra)
-		qs.Set("limit", "1")
-		qs.Set("tags", strings.Join(tags, ","))
-
-		u.RawQuery = qs.Encode()
-
-		var body []byte
-
-		b := backoff.NewExponentialBackOff()
-		b.MaxInterval = 10 * time.Second
-		b.MaxElapsedTime = time.Minute
-
-		err := backoff.Retry(func() (err error) {
-			resp, err := http.Get(u.String())
-			if err != nil {
-				return err
-			}
-			defer resp.Body.Close()
-			body, err = ioutil.ReadAll(resp.Body)
-			return
-		}, b)
-
-		if err != nil {
-			return "", err
-		}
-
-		imageResp := &apiSelectorImageResponse{
-			Data: []*apiSelectorImageRef{},
-		}
-
-		err = json.Unmarshal(body, imageResp)
-		if err != nil {
-			return "", err
-		}
-
-		if len(imageResp.Data) > 0 {
-			return imageResp.Data[0].Name, nil
-		}
-	}
-	return imageName, nil
+	return append([][]string{fullTagSet}, candidateTags...)
 }
 
 type apiSelectorImageResponse struct {
--- src/github.com/travis-ci/worker/image/api_selector_test.go
+++ src/github.com/travis-ci/worker/image/api_selector_test.go
@@ -22,6 +22,110 @@ const (
 	]
 }
 `
+	testAPIServerEmptyResponseString = `{"data": []}`
+)
+
+var (
+	testAPITagTestCases = []struct {
+		P []*Params
+		E [][][]string
+	}{
+		{
+			P: []*Params{
+				&Params{
+					Infra:    "gce",
+					Language: "ruby",
+					OS:       "linux",
+				},
+				&Params{
+					Infra:    "gce",
+					Language: "go",
+					OS:       "linux",
+					Group:    "dev",
+				},
+				&Params{
+					Infra:    "macstadium6",
+					Language: "python",
+					OS:       "osx",
+					OsxImage: "xcode7",
+				},
+				&Params{
+					Infra:    "macstadium6",
+					Language: "objective-c",
+					OS:       "osx",
+					OsxImage: "xcode6.4",
+				},
+				&Params{
+					Infra:    "macstadium6",
+					Language: "node_js",
+					OsxImage: "xcode6.1",
+					Dist:     "yosammity",
+					Group:    "fancy",
+					OS:       "osx",
+				},
+			},
+			E: [][][]string{
+				[][]string{
+					[]string{"language:ruby", "language_ruby:true", "os:linux"},
+					[]string{"os:linux", "language:ruby"},
+					[]string{"os:linux", "language_ruby:true"},
+					[]string{"language:ruby"},
+					[]string{"language_ruby:true"},
+					[]string{"os:linux"},
+				},
+				[][]string{
+					[]string{"language:go", "language_go:true", "group:dev", "os:linux"},
+					[]string{"group:dev", "language:go"},
+					[]string{"group:dev", "language_go:true"},
+					[]string{"os:linux", "language:go"},
+					[]string{"os:linux", "language_go:true"},
+					[]string{"language:go"},
+					[]string{"language_go:true"},
+					[]string{"group:dev"},
+					[]string{"os:linux"},
+				},
+				[][]string{
+					[]string{"language:python", "language_python:true", "osx_image:xcode7", "os:osx"},
+					[]string{"osx_image:xcode7", "language:python"},
+					[]string{"osx_image:xcode7", "language_python:true"},
+					[]string{"os:osx", "language:python"},
+					[]string{"os:osx", "language_python:true"},
+					[]string{"language:python"},
+					[]string{"language_python:true"},
+					[]string{"osx_image:xcode7"},
+					[]string{"os:osx"},
+				},
+				[][]string{
+					[]string{"language:objective-c", "language_objective-c:true", "osx_image:xcode6.4", "os:osx"},
+					[]string{"osx_image:xcode6.4", "language:objective-c"},
+					[]string{"osx_image:xcode6.4", "language_objective-c:true"},
+					[]string{"os:osx", "language:objective-c"},
+					[]string{"os:osx", "language_objective-c:true"},
+					[]string{"language:objective-c"},
+					[]string{"language_objective-c:true"},
+					[]string{"osx_image:xcode6.4"},
+					[]string{"os:osx"},
+				},
+				[][]string{
+					[]string{"language:node_js", "language_node_js:true", "osx_image:xcode6.1", "dist:yosammity", "group:fancy", "os:osx"},
+					[]string{"osx_image:xcode6.1", "language:node_js"},
+					[]string{"osx_image:xcode6.1", "language_node_js:true"},
+					[]string{"dist:yosammity", "language:node_js"},
+					[]string{"dist:yosammity", "language_node_js:true"},
+					[]string{"group:fancy", "language:node_js"},
+					[]string{"group:fancy", "language_node_js:true"},
+					[]string{"os:osx", "language:node_js"},
+					[]string{"os:osx", "language_node_js:true"},
+					[]string{"language:node_js"},
+					[]string{"language_node_js:true"},
+					[]string{"osx_image:xcode6.1"},
+					[]string{"dist:yosammity"},
+					[]string{"group:fancy"},
+					[]string{"os:osx"},
+				},
+			},
+		},
+	}
 )
 
 func TestNewAPISelector(t *testing.T) {
@@ -48,4 +152,44 @@ func TestAPISelector_Select(t *testing.T) {
 		OS:       "osx",
 	})
 	assert.Equal(t, actual, "travis-ci-awesome")
+}
+
+func TestAPISelector_SelectDefault(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, testAPIServerEmptyResponseString)
+	}))
+	defer ts.Close()
+	u, _ := url.Parse(ts.URL)
+	actual, _ := NewAPISelector(u).Select(&Params{})
+	assert.Equal(t, actual, "default")
+}
+
+func TestAPISelector_SelectDefaultWhenBadResponse(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer ts.Close()
+	u, _ := url.Parse(ts.URL)
+	actual, _ := NewAPISelector(u).Select(&Params{})
+	assert.Equal(t, actual, "default")
+}
+
+func TestAPISelector_SelectDefaultWhenBadJSON(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, `{"data`)
+	}))
+	defer ts.Close()
+	u, _ := url.Parse(ts.URL)
+	actual, _ := NewAPISelector(u).Select(&Params{})
+	assert.Equal(t, actual, "default")
+}
+
+func TestAPISelector_buildCandidateTags(t *testing.T) {
+	as := NewAPISelector(nil)
+
+	for _, tc := range testAPITagTestCases {
+		for i, params := range tc.P {
+			assert.Equal(t, tc.E[i], as.buildCandidateTags(params))
+		}
+	}
 }
