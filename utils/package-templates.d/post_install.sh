a3e42d99a9a2c0a14381071e5252b0cd410a96cd
Dan Buch <dan@travis-ci.org>
2015-11-24 21:02:45 -0500

    Hardening some of the package hook scripts

--- utils/package-templates.d/post_install.sh
+++ utils/package-templates.d/post_install.sh
@@ -1,8 +1,17 @@
 # travis-worker post-install script
 
-if ! getent passwd travis >/dev/null ; then
-  useradd -m -r travis -c "travis for travis-worker"
-fi
+__add_travis_user() {
+  if getent passwd travis >/dev/null ; then
+    return
+  fi
 
-mkdir -p /var/tmp/travis-run.d
-chown -R travis:travis /var/tmp/travis-run.d
+  useradd -m -r travis -c "travis for travis-worker"
+}
+
+__create_travis_run_dir() {
+  mkdir -p /var/tmp/travis-run.d
+  chown -R travis:travis /var/tmp/travis-run.d
+}
+
+__add_travis_user
+__create_travis_run_dir
--- utils/package-templates.d/pre_remove.sh
+++ utils/package-templates.d/pre_remove.sh
@@ -1,7 +1,36 @@
-# pre-install script for travis-worker
+# travis-worker pre-remove script
 
-if getent passwd travis >/dev/null ; then
-  userdel travis -r 2>/dev/null || {
-    echo "Failed to remove travis user (exit $?)"
-  }
-fi
+__stop_travis_worker() {
+  if ! status travis-worker >/dev/null 2>&1 ; then
+    return
+  fi
+
+  if stop travis-worker ; then
+    return
+  fi
+
+  echo "Failed to stop travis-worker (exit $?)"
+  echo 'Sending SIGKILL'
+  killall -9 travis-worker
+
+  if stop travis-worker ; then
+    return
+  fi
+
+  echo "Failed to stop travis-worker after sending SIGKILL (exit $?)"
+}
+
+__remove_travis_user() {
+  if ! getent passwd travis >/dev/null 2>&1 ; then
+    return
+  fi
+
+  if userdel travis -r 2>/dev/null ; then
+    return
+  fi
+
+  echo "Failed to remove travis user (exit $?)"
+}
+
+__remove_travis_user
+__stop_travis_worker
