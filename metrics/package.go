dfc1a6f7d029486297444bb3acee1c334e1fa869
Igor Wiedler <igor@travis-ci.org>
2017-02-09 15:06:35 +0100

    metrics: reduce sample size of timer ExpDecaySample to have less skewed timer metrics

--- metrics/package.go
+++ metrics/package.go
@@ -14,15 +14,29 @@ func Mark(name string) {
 
 // TimeSince increases the timer metric with the given name by the time since the given time
 func TimeSince(name string, since time.Time) {
+	metrics.DefaultRegistry.Register(name, newTimerWithSmallerSampleSize)
+
 	metrics.GetOrRegisterTimer(name, metrics.DefaultRegistry).UpdateSince(since)
 }
 
 // TimeDuration increases the timer metric with the given name by the given duration
 func TimeDuration(name string, duration time.Duration) {
+	metrics.DefaultRegistry.Register(name, newTimerWithSmallerSampleSize)
+
 	metrics.GetOrRegisterTimer(name, metrics.DefaultRegistry).Update(duration)
 }
 
 // Gauge sets a gauge metric to a given value
 func Gauge(name string, value int64) {
 	metrics.GetOrRegisterGauge(name, metrics.DefaultRegistry).Update(value)
+}
+
+// the default Timer implementation from go-metrics has a large sample size of 1028
+// this skews the results towards high values when we don't have that much throughput
+// to get more accurate percentiles in worker, we will use a smaller sample size of 50
+func newTimerWithSmallerSampleSize() metrics.Timer {
+	return metrics.NewCustomTimer(
+		metrics.NewHistogram(metrics.NewExpDecaySample(50, 0.015)),
+		metrics.NewMeter(),
+	)
 }
