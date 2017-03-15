7dd0160bcb364a243aa034adc77e1e605a616ab8
Henrik Hodne <henrik@travis-ci.com>
2017-03-15 15:06:09 +0100

    Allow multiple cancellation subscriptions for same job ID
    Prior to this, you could have a job get requeued back to the same worker
    immediately after an error, but since there are some cleanup steps
    between the requeue and when the job actually "finishes" in worker, and
    the "unsubscribe cancellation" step is done last, you could then have a
    situation where a new job is redelivered to the same worker before the
    old processor has unsubscribed.
    
    This commit also uses the opportunity to refactor the cancellation logic
    a bit and pull the "broadcast" part to a separate type instead of
    embedding it in the old Canceller interface and requiring each
    implementor to implement the semantics correctly.
    
    This makes it easier to test the semantics of cancellation without
    setting up a "Canceller", you just use the in-memory cancellation
    broadcaster and send cancellation broadcasts to it. All the
    AMQPCanceller type does now is listen to AMQP commands and forward
    cancellations to the CancellationBroadcaster.
    
    The FileCanceller was removed since it was a no-op and there's no longer
    a need for something to implement the Canceller interface.
    
    Fixes #288.

---
+++ canceller_test.go
@@ -0,0 +1,45 @@
+package worker
+
+import "testing"
+
+func TestCancellationBroadcaster(t *testing.T) {
+	cb := NewCancellationBroadcaster()
+
+	ch1_1 := cb.Subscribe(1)
+	ch1_2 := cb.Subscribe(1)
+	ch1_3 := cb.Subscribe(1)
+	ch2 := cb.Subscribe(2)
+
+	cb.Unsubscribe(1, ch1_2)
+
+	cb.Broadcast(1)
+	cb.Broadcast(1)
+
+	assertClosed(t, "ch1_1", ch1_1)
+	assertWaiting(t, "ch1_2", ch1_2)
+	assertClosed(t, "ch1_3", ch1_3)
+	assertWaiting(t, "ch2", ch2)
+}
+
+func assertClosed(t *testing.T, name string, ch <-chan struct{}) {
+	select {
+	case _, ok := (<-ch):
+		if ok {
+			t.Errorf("expected %s to be closed, but it received a value", name)
+		}
+	default:
+		t.Errorf("expected %s to be closed, but it wasn't", name)
+	}
+}
+
+func assertWaiting(t *testing.T, name string, ch <-chan struct{}) {
+	select {
+	case _, ok := (<-ch):
+		if ok {
+			t.Errorf("expected %s to be not be closed and not have a value, but it received a value", name)
+		} else {
+			t.Errorf("expected %s to be not be closed and not have a value, but it was closed", name)
+		}
+	default:
+	}
+}
