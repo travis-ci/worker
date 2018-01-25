2989520e71f3ed1cc7ad512f1bb8607d503d281d
Jan Schulte <hello@unexpected-co.de>
2018-01-25 18:08:44 +0100

    Also build for Ubuntu 16.04

--- packaging/config.bash
+++ packaging/config.bash
@@ -11,7 +11,7 @@ export DATE
 export PC_USER='travisci'
 export PC_REPO='worker'
 
-export PKG_PLATFORMS=('ubuntu:14.04' 'centos:7')
+export PKG_PLATFORMS=('ubuntu:14.04' 'centos:7' 'ubuntu:16.04')
 
 __define_version
 __define_shell_flags
