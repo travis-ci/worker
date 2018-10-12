b786d13133ef787c8ebb9741c220438ae6e77540
Lyoness <carmen.andoh@gmail.com>
2018-10-12 09:15:09 -0400

    Aint nobody got time for needless type coercion

--- processor.go
+++ processor.go
@@ -187,12 +187,11 @@ func (p *Processor) process(ctx gocontext.Context, buildJob Job) {
 	defer span.End()
 
 	span.AddAttributes(
-
 		trace.StringAttribute("app", "worker"),
 		trace.Int64Attribute("job_id", int64(buildJob.Payload().Job.ID)),
-		trace.StringAttribute("repo", string(buildJob.Payload().Repository.Slug)),
-		trace.StringAttribute("infra", string(p.config.ProviderName)),
-		trace.StringAttribute("site", string(p.config.TravisSite)),
+		trace.StringAttribute("repo", buildJob.Payload().Repository.Slug),
+		trace.StringAttribute("infra", p.config.ProviderName),
+		trace.StringAttribute("site", p.config.TravisSite),
 	)
 
 	state := new(multistep.BasicStateBag)
