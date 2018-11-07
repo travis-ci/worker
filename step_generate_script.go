8ded6aa50c99bf5eff18ddff5ed2ff60b8027847
Igor Wiedler <igor@travis-ci.org>
2018-11-07 14:39:55 +0100

    also add span result in step_generate_script

--- step_generate_script.go
+++ step_generate_script.go
@@ -47,6 +47,11 @@ func (s *stepGenerateScript) Run(state multistep.StateBag) multistep.StepAction 
 	if err != nil {
 		state.Put("err", err)
 
+		span.SetStatus(trace.Status{
+			Code:    trace.StatusCodeUnavailable,
+			Message: err.Error(),
+		})
+
 		logger.WithField("err", err).Error("couldn't generate build script, erroring job")
 		err := buildJob.Error(ctx, "An error occurred while generating the build script.")
 		if err != nil {
