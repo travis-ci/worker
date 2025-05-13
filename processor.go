724f6e8620ae6bb22eead2f28333e226a02ea7b5
Piotr Milcarz <piotr.milcarz@gmail.com>
2025-05-13 21:25:17 +0200

    test

--- processor.go
+++ processor.go
@@ -231,7 +231,7 @@ func (p *Processor) process(ctx gocontext.Context, buildJob Job) {
 		"job_id": buildJob.Payload().Job.ID,
 		"self":   "processor",
 	})
-	logger.Info(fmt.Sprintf("DEBUGDEBUG process buildJob.Payload(): %v", buildJob.Payload()))
+	logger.Info(fmt.Sprintf("DEBUGDEBUG process buildJob.Payload(): %v", buildJob.Payload().Job))
 	logger.Info(fmt.Sprintf("DEBUGDEBUG process buildJob.Payload().TriggererId: %d", buildJob.Payload().TriggererId))
 	logger.Info(fmt.Sprintf("DEBUGDEBUG process buildJob.Payload().CreatedCustomImage.Owner.Id: %d", buildJob.Payload().Job.CreatedCustomImage.Owner.Id))
 	logger.Info(fmt.Sprintf("DEBUGDEBUG process buildJob.Payload().CreatedCustomImage.Owner.Type: %s", buildJob.Payload().Job.CreatedCustomImage.Owner.Type))
