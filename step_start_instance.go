6ca18034b2118ecc95ad63e6a752a1ec5fc9c806
Piotr Milcarz <piotr.milcarz@gmail.com>
2025-05-19 14:21:48 +0200

    fix

--- step_start_instance.go
+++ step_start_instance.go
@@ -75,8 +75,9 @@ func (s *stepStartInstance) Run(state multistep.StateBag) multistep.StepAction {
 	buildJob.StartAttributes().UsedCustomImageId = usedCustomImageId
 	buildJob.StartAttributes().UsedCustomImageName = usedCustomImageName
 
-	fmt.Fprintf(logWriter, "Using custom build environment image %s %s. See <link to the documentation>.", usedCustomImageName, s.artifactManager.GenerateCustomImageName(ownerId, ownerType, usedCustomImageId))
-
+	if usedCustomImageId != 0 {
+		fmt.Fprintf(logWriter, "Using custom build environment image %s %s. See <link to the documentation>.", usedCustomImageName, s.artifactManager.GenerateCustomImageName(ownerId, ownerType, usedCustomImageId))
+	}
 	if s.provider.SupportsProgress() && buildJob.StartAttributes().ProgressType != "" {
 		var progresser backend.Progresser
 		switch buildJob.StartAttributes().ProgressType {
