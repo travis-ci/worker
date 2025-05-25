5ba32ec8295051a7152e9dee641535559d1e7f1e
Piotr Milcarz <piotr.milcarz@gmail.com>
2025-05-25 15:51:01 +0200

    test

--- step_write_worker_info.go
+++ step_write_worker_info.go
@@ -34,7 +34,7 @@ func (s *stepWriteWorkerInfo) Run(state multistep.StateBag) multistep.StepAction
 		}, "\n")))
 		if usedCustomImageId != 0 {
 			writeSingleLine(logWriter, []byte(strings.Join([]string{
-				fmt.Sprintf("[OK \033[33;1mUsing custom build environment image %s %s. See <link to the documentation>.\033[0m", usedCustomImageName, instance.ImageName()),
+				fmt.Sprintf("Using custom build environment image %s %s. See <link to the documentation>.", usedCustomImageName, instance.ImageName()),
 			}, "\n")))
 		}
 	}
